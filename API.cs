c01f409b0b015370e822517bf6fab7f59cf2b90f
Timo Zuccarello <timo@zuccarello.eu>
2024-04-26 21:57:15 +0200

    Add command to start and stop time.

--- API.cs
+++ API.cs
@@ -1,27 +1,102 @@
-public class API : IModApi
+using System.Collections.Generic;
+
+public class SmartTime : IModApi
 {
+    public enum SmartTimeOverride {
+        DEFAULT,
+        FREEZE,
+        UNFREEZE
+    }
+
+    public static SmartTimeOverride Overridden = SmartTimeOverride.DEFAULT;
     int minNumberOfPlayers = 4;
     ulong lastWorldTime = 0;
+
     public void InitMod(Mod mod)
     {
-       ModEvents.GameUpdate.RegisterHandler(GameUpdate);
+        ModEvents.GameUpdate.RegisterHandler(GameUpdate);
+        // Maybe this happens automatically already?!
+        // SingletonMonoBehaviour<SdtdConsole>.Instance.RegisterCommands();
+
+    }
+
+    private bool ShouldTimeProgress() {
+        if (Overridden == SmartTimeOverride.FREEZE) {
+            return false;
+        } else if (Overridden == SmartTimeOverride.UNFREEZE) {
+            return true;
+        }
+
+        World world = GameManager.Instance.World;
+
+        return world.Players.list.Count >= minNumberOfPlayers;
+    }
+
+    private bool UpdateWorldTime() {
+        World world = GameManager.Instance.World;
+        if (lastWorldTime == 0) {
+            lastWorldTime = world.worldTime;
+        }
+
+        if (!ShouldTimeProgress()) {
+            world.SetTimeJump(lastWorldTime, false);
+            return false;
+        } else {
+            lastWorldTime = world.worldTime;
+            return true;
+        }
     }
 
     public void GameUpdate()
     {
         World world = GameManager.Instance.World;
-        
-        if (lastWorldTime == 0) {
-            lastWorldTime = world.worldTime;
-            return;
+        if (world.Players.list.Count == 0 && Overridden != SmartTimeOverride.DEFAULT) {
+            // Reset override as soon as all players are disconnected.
+            Overridden = SmartTimeOverride.DEFAULT;
         }
 
-        int playerCount = world.Players.list.Count;
+        UpdateWorldTime();
+    }
 
-        if (playerCount < minNumberOfPlayers) {
-            world.SetTimeJump(lastWorldTime, false);
-        }
+    public static void ForceFreeze() {
+        Overridden = SmartTimeOverride.FREEZE;
+    }
 
-        lastWorldTime = world.worldTime;
+    public static void ForceUnfreeze() {
+        Overridden = SmartTimeOverride.UNFREEZE;
+    }
+}
+
+public class ConsoleCmdStartTime : ConsoleCmdAbstract
+{
+    public override int DefaultPermissionLevel => 0;
+
+    protected override string[] getCommands() {
+        return new string[1]{ "starttime" };
+    }
+
+    protected override string getDescription() {
+        return "Starts the time until all players are disconnected.";
+    }
+
+    public override void Execute(List<string> _params, CommandSenderInfo _senderInfo) {
+        SmartTime.ForceUnfreeze();
+    }
+}
+
+public class ConsoleCmdStopTime : ConsoleCmdAbstract
+{
+    public override int DefaultPermissionLevel => 0;
+
+    protected override string[] getCommands() {
+        return new string[1]{ "stoptime" };
+    }
+
+    protected override string getDescription() {
+        return "Stops the time until all players are disconnected.";
+    }
+
+    public override void Execute(List<string> _params, CommandSenderInfo _senderInfo) {
+        SmartTime.ForceFreeze();
     }
 }
